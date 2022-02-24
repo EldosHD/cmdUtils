3a942bb300e47d80fb68f1b4bb7e2b90d2da9fd8
EldosHD <Valentin.Reichenbach@live.com>
2022-02-24 14:40:04 +0100

    added colorsnitch file

---
+++ colorSnitch/colorSnitch.py
@@ -0,0 +1,78 @@
+import sys
+import re
+import argparse
+
+
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+def calcToDoPrio(inputString: str):
+    return str(inputString.count('O') - 1)
+
+
+parser = argparse.ArgumentParser(description='TODO: describe that')
+
+# TODOOOOO: make this program run snitch if it doesnt get input
+parser.add_argument('-r','--run', action='store_true', default=False, help='run "snitch list" and use it as input')
+parser.add_argument('--count', action='store_true', default=False, help='Print the priority')
+
+
+args = parser.parse_args()
+
+
+snitchOutput = []
+for line in sys.stdin:
+    snitchOutput.append(line.replace('\n',''))
+
+for line in snitchOutput:
+    toDo = line.split(':')
+    
+    fileName = color.CYAN + toDo[0] + color.END
+    lineNumber = color.RED + toDo[1] + color.END
+
+    # remove filename and linenumber from that list
+    toDo.pop(0)
+    toDo.pop(0)
+
+    for index, word in enumerate(toDo):
+        if re.search('TODOOO+',word):
+            if args.count == True:
+                newWord = color.RED + calcToDoPrio(word) + color.END
+            else:
+                newWord = re.search('TODOOO+', word)
+                newWord = color.RED + newWord.group() + color.END
+            toDo[index] = newWord
+            break
+        elif re.search('TODOO',word):
+            if args.count == True:
+                newWord = color.YELLOW + calcToDoPrio(word) + color.END
+            else:
+                newWord = re.search('TODOO', word)
+                newWord = color.YELLOW + newWord.group() + color.END
+            toDo[index] = newWord
+            break
+        elif re.search('TODO', word):
+            if args.count == True:
+                newWord = color.GREEN + calcToDoPrio(word) + color.END
+            else:
+                newWord = re.search('TODO', word)
+                newWord = color.GREEN + newWord.group() + color.END
+            toDo[index] = newWord
+            break
+    # stitch the colored todo str back together
+    line = ':'.join(toDo)
+    print(fileName + ':' + lineNumber + ':' + line)
+
+    
+# TODOO: only supports the word "TODO" currently. should support others too! <--------------------- this line is buggy
+# TODOO: remove unnessesary whitespace from each line
+# TODOO: make the program format snitch nicely
